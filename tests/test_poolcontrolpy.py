c3145eb4d0bb1aa58b428143aba7bbb7fd18a58b
geeto1969 <1621095+geeto1969@users.noreply.github.com>
2022-08-01 02:40:14 +0000

    feat: add config fetching

--- tests/test_poolcontrolpy.py
+++ tests/test_poolcontrolpy.py
@@ -49,3 +49,23 @@ async def test_checkconnect_failed(aiohttp_server):
             assert False, 'Raised unexpected Exception'
         else:
             assert not resp, 'Returned True on bad config'
+
+async def test_fetchconfig_good(aiohttp_server):
+    """Test successful connection to controller
+    """
+    # Setup test server
+    app = aiohttp.web.Application()
+    app.add_routes(routes)
+    server = await aiohttp_server(app)
+
+    # Confirm fetchconfig populates correct values
+    async with aiohttp.ClientSession() as client:
+        controller = Controller(client, server.host, server.port)
+        try:
+            resp = await controller.fetchconfig()
+        except Exception: # pylint: disable=broad-except
+            assert False, 'Raised unexpected Exception'
+        else:
+            assert controller.type == "easytouch", f'Returned "{controller.type}" and expected "easytouch"'
+            assert controller.model == "EasyTouch2 4", f'Returned "{controller.model}" and expected "EasyTouch2 4"'
+            assert controller.version == "7.6.0", f'Returned "{controller.version}" and expected "7.6.0"'
